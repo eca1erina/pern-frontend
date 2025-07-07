8b6864321eaa2f954ac87b56f75eed2ebcee78c1
zillenya <jasmine.afrem@elenacuza.ro>
2025-07-07 13:29:53 +0300

    Deleted some files

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -4,15 +4,17 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <Link href="/" className="logoContainer">
-            <Image
-                src="/logoSmall.png"
-                alt="Logo"
-                width={40}
-                height={40}
-                className="logoImage"
-            />
-            <span className="logoText">MyApp</span>
+        <Link href="/">
+            <div className="logoContainer">
+                <Image
+                    src="/logoSmall.png"
+                    alt="Logo"
+                    width={40}
+                    height={40}
+                    className="logoImage"
+                />
+                <span className="logoText">Wise Tracker</span>
+            </div>
         </Link>
     );
 };
